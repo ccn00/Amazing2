7cd4c2b89c915b8b456ce319b55e4308b6644425
Juan Carlos Castro <59028123+ccn00@users.noreply.github.com>
2022-11-19 23:30:30 +0100

    Creando pagina principal de la tienda, xml de carga cuando abrimos la aplicación y añadiendo imagenes para producto.

--- app/src/main/java/com/example/amazing2/MainActivity.kt
+++ app/src/main/java/com/example/amazing2/MainActivity.kt
@@ -7,14 +7,23 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.amazing2.data.AppDatabase
 import com.example.amazing2.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
--- app/src/main/java/com/example/amazing2/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/amazing2/ui/home/HomeViewModel.kt
@@ -4,6 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+
+// La clase ViewModel se usa para almacenar y administrar datos relacionados con la IU.
+// La IU se puede vincular a datos observables almacenados en el ViewModel y se puede
+// notificar a la IU cuando los datos cambian.
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
