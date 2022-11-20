53a3097bbc82907519d3f1a739f8f3206a49d6cd
Juan Carlos Castro <59028123+ccn00@users.noreply.github.com>
2022-11-20 18:18:38 +0100

    Cambiando nombre de elementos relacionados con dashboard -> carrito y notifications -> perfil.

--- app/src/main/java/com/example/amazing2/ui/carrito/CarritoViewModel.kt
+++ app/src/main/java/com/example/amazing2/ui/carrito/CarritoViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.amazing2.ui.notifications
+package com.example.amazing2.ui.carrito
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class CarritoViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/amazing2/ui/notifications/PerfilFragment.kt
+++ app/src/main/java/com/example/amazing2/ui/notifications/PerfilFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.amazing2.databinding.FragmentNotificationsBinding
+import com.example.amazing2.databinding.FragmentPerfilBinding
 
-class NotificationsFragment : Fragment() {
+class PerfilFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentPerfilBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val perfilViewModel =
+            ViewModelProvider(this).get(PerfilViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentPerfilBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        perfilViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
