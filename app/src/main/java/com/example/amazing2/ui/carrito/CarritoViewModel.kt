5f15f155aa0eac780a02925dd537f1777b763cb1
Juan Carlos Castro <59028123+ccn00@users.noreply.github.com>
2022-11-20 19:12:31 +0100

    Modificando pantalla carrito, añadiendo carrito vacio.

--- app/src/main/java/com/example/amazing2/ui/carrito/CarritoViewModel.kt
+++ app/src/main/java/com/example/amazing2/ui/carrito/CarritoViewModel.kt
@@ -3,11 +3,18 @@ package com.example.amazing2.ui.carrito
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.amazing2.R
 
 class CarritoViewModel : ViewModel() {
 
+    // Queremos hacer un textView el cual sera rellenado con un string que se encuentra en el archivo strings.xml
+
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is carrito Fragment"
     }
     val text: LiveData<String> = _text
+
+
+
+
 }
