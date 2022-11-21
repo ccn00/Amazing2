bd427db6c17556efad7a86db331ecb0ef970aef1
Juan Carlos Castro <59028123+ccn00@users.noreply.github.com>
2022-11-21 01:02:10 +0100

    Añadiendo logos para perfil element e incluyendolos en el fragment, cambio de color de logo y flecha.

--- app/src/main/java/com/example/amazing2/ui/notifications/PerfilViewModel.kt
+++ app/src/main/java/com/example/amazing2/ui/notifications/PerfilViewModel.kt
@@ -3,11 +3,16 @@ package com.example.amazing2.ui.notifications
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.amazing2.adapter.PerfilElement
+import com.example.amazing2.adapter.ProductoElement
 
 class PerfilViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    private val _recycler = MutableLiveData<List<PerfilElement>>().apply {
+        value = listOf()
     }
-    val text: LiveData<String> = _text
+    val recycler: LiveData<List<PerfilElement>> = _recycler
+
+
+
 }
