19acd5dfe6b92d2b9d1e8eb93f0c9b7b255e98c4
JUAN CARLOS CASTRO NAVARRO <JuanCarlos.Castro1@alu.uclm.es>
2022-11-21 10:32:05 +0100

    Borrando descripción en profile_element, fragment_perfil cambiando nombre de notifications. Añadiendo tilde a carrito vacío. Actualizado build.gradle.

--- app/src/main/java/com/example/amazing2/ui/perfil/PerfilFragment.kt
+++ app/src/main/java/com/example/amazing2/ui/perfil/PerfilFragment.kt
@@ -1,4 +1,4 @@
-package com.example.amazing2.ui.notifications
+package com.example.amazing2.ui.perfil
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,7 +6,6 @@ import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
--- app/src/main/java/com/example/amazing2/ui/perfil/PerfilViewModel.kt
+++ app/src/main/java/com/example/amazing2/ui/perfil/PerfilViewModel.kt
@@ -1,10 +1,9 @@
-package com.example.amazing2.ui.notifications
+package com.example.amazing2.ui.perfil
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.amazing2.adapter.PerfilElement
-import com.example.amazing2.adapter.ProductoElement
 
 class PerfilViewModel : ViewModel() {
 
